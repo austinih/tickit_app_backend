cc916e308c7d451730f08b0c487e1f8298d3cdaa
Toni <windtronic89@gmail.com>
2023-04-01 09:38:03 -0600

    routes for venues and events I think?

--- tickit_project/tickit/urls.py
+++ tickit_project/tickit/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('tickets', views.TicketList.as_view(), name='ticket_list'),
     path('venues/<int:pk>', views.VenueDetail.as_view(), name='venue_detail'),
     path('events/<int:pk>', views.EventDetail.as_view(), name='event_detail'),
-    path('tickets/<int:pk>', views.TicketDetail.as_view(), name='ticket_detail')
+    path('tickets/<int:pk>', views.TicketDetail.as_view(), name='ticket_detail'),
+    path('venues/<int:venue_pk>/events/<int:event_pk>', views.EventVenueDetail.as_view(), name='event_venue_detail')
+
 ]
+
--- tickit_project/tickit/views.py
+++ tickit_project/tickit/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework import generics
 from .serializers import VenueSerializer, EventSerializer, TicketSerializer
 from .models import Venue, Event, Ticket
+from django.views.generic import View
+
 
 class VenueList(generics.ListCreateAPIView):
     queryset = Venue.objects.all()
@@ -18,6 +20,13 @@ class EventList(generics.ListCreateAPIView):
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer 
+    
+class EventVenueDetail(View):
+    def get(self, request, venue_pk, event_pk):
+        venue = get_object_or_404(Venue, pk=venue_pk)
+        event = get_object_or_404(Event, pk=event_pk)
+        context = {'venue': venue, 'event': event}
+        return render(request, 'event_venue_detail.html', context)
     
 class TicketList(generics.ListCreateAPIView):
     queryset = Ticket.objects.all()
